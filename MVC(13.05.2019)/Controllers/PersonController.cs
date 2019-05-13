b7f24f45e4d585e522cbf8ec1584f5ec4e35e73b
Goktug Ercaliskan <goktug.ercaliskan@BK-LAB204-IS025>
2019-05-13 12:34:47 +0300

    .

---
+++ MVC(13.05.2019)/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using MVC_13._05._2019_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_13._05._2019_.Controllers
+{
+    public class PersonController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            PersonModel model = new PersonModel();
+            model.Name = "Göktuğ Erçalışkan";
+            model.BirthDate = new DateTime(1989,10,14);
+
+            model.GsmNumberList = new List<string>();
+            model.GsmNumberList.Add("8181818");
+            model.GsmNumberList.Add("55511115151");
+            model.GsmNumberList.Add("895445621");
+
+
+
+            model.AdressList = new List<string>();
+            model.AdressList.Add("Mahmut tuncer sokak");
+            model.AdressList.Add("sokak cadde");
+
+
+
+
+
+            return View(model);
+        }
+    }
+}
---
+++ MVC(13.05.2019)/Models/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_13._05._2019_.Models
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List <string> GsmNumberList { get; set; }
+        public List<string> AdressList { get; set; }
+
+
+
+
+    }
+}
